25e572bd8cb65ed70b9ca96e89c486409f486778
Jsy <tjsus247@naver.com>
2026-05-19 17:00:35 +0900

    폴더구조 수정

--- src/shared/components/common/Sidebar.tsx
+++ src/shared/components/common/Sidebar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { LuLayoutDashboard, LuFlaskConical } from "react-icons/lu";
-import { useSidebarStore } from "../../stores/sidebarStore";
+import { useSidebarStore } from "@/shared/stores/sidebarStore";
 
 type MenuItem = {
   label: string;
